59bd2ab13779a5fb15ac5eb9f35c9875d2ef037e
himanish58 <himanish.kabiraj@gmail.com>
2019-08-04 02:27:00 +0530

    Using hardcoded data because of no-cors error

--- src/requests/movies.js
+++ src/requests/movies.js
@@ -1,22 +1,28 @@
-export default function getMovies(payload) {
-	return new Promise((resolve, reject) => {
-		let data = {
-			Accept: 'application/json',
-			'Content-Type': 'application/json'
-		};
+import Data from './data';
 
-		let myInit = {
-			method: 'GET',
-			mode: 'no-cors',
-			header: data
-		};
-		fetch('https://in.bookmyshow.com/serv/getData?cmd=GETTRAILERS&mtype=cs', myInit).then(
-			(response) => {
-				if (response.ok) {
-					return resolve(response.body);
-				}
-			},
-			(error) => reject(error)
-		);
-	});
+// export default function getMovies(payload) {
+// 	return new Promise((resolve, reject) => {
+// 		let data = {
+// 			Accept: 'application/json',
+// 			'Content-Type': 'application/json'
+// 		};
+
+// 		let myInit = {
+// 			method: 'GET',
+// 			mode: 'no-cors',
+// 			header: data
+// 		};
+// 		fetch('https://in.bookmyshow.com/serv/getData?cmd=GETTRAILERS&mtype=cs', myInit).then(
+// 			(response) => {
+// 				if (response.ok) {
+// 					return resolve(response.body);
+// 				}
+// 			},
+// 			(error) => reject(error)
+// 		);
+// 	});
+// }
+
+export default function getMovie() {
+	return new Promise((resolve, reject) => resolve(Data));
 }
