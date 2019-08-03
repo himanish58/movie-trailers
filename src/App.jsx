90a5364d838a95c8b4f9883b144311b43f274263
himanish58 <himanish.kabiraj@gmail.com>
2019-08-04 01:11:01 +0530

    Some cleanup in boiler plate and created basic structure for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import getMovies from './requests/movies';
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+
+	componentDidMount() {
+		getMovies()
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((error) => {
+				console.error(`Error in App.componentDidMount.getMovies: ${error}`);
+			});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<h1>Hello World</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/requests/movies.js
@@ -0,0 +1,22 @@
+export default function getMovies(payload) {
+	return new Promise((resolve, reject) => {
+		let data = {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		};
+
+		let myInit = {
+			method: 'GET',
+			mode: 'no-cors',
+			header: data
+		};
+		fetch('https://in.bookmyshow.com/serv/getData?cmd=GETTRAILERS&mtype=cs', myInit).then(
+			(response) => {
+				if (response.ok) {
+					return resolve(response.body);
+				}
+			},
+			(error) => reject(error)
+		);
+	});
+}
