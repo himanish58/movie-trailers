6dc410ec1c3b8460bb8994d30388acf2e2311909
himanish58 <himanish.kabiraj@gmail.com>
2019-08-05 09:35:06 +0530

    Added buld folder

---
+++ docs/precache-manifest.b955280d3df2641c024f9f712f474317.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c54c410272c351af00a3324f6300907",
+    "url": "/movie-trailers/index.html"
+  },
+  {
+    "revision": "cc2bfda192e3d9a8e387",
+    "url": "/movie-trailers/static/css/main.9d646ed0.chunk.css"
+  },
+  {
+    "revision": "e1eef6ab70acd56aec52",
+    "url": "/movie-trailers/static/js/2.2bbf759b.chunk.js"
+  },
+  {
+    "revision": "cc2bfda192e3d9a8e387",
+    "url": "/movie-trailers/static/js/main.8a1fc7b3.chunk.js"
+  },
+  {
+    "revision": "c6a449016d482728c6ee",
+    "url": "/movie-trailers/static/js/runtime~main.9ac1d178.js"
+  }
+]);
